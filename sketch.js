751473cdde7aaedb0a1b08223e13b936011c4147
elisamanzoni <37081924+elisamanzoni@users.noreply.github.com>
2020-07-03 12:47:30 +0200

    tendina

--- sketch.js
+++ sketch.js
@@ -1,11 +1,23 @@
-function preload(){
+function preload() {
   // put preload code here
 }
 
 function setup() {
+
+  var olympic = select('#container_olympic');
+  olympic.mouseClicked(olympic_open);
   // put setup code here
 }
 
 function draw() {
   // put drawing code here
+}
+
+function olympic_open() {
+  var description_olympic = select('#description_olympic');
+  if (description_olympic.hasClass('display_none')) {
+    description_olympic.removeClass('display_none');
+  } else {
+    description_olympic.addClass('display_none')
+  }
 }
